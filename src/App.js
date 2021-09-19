269c1b163e77e189525af4a57719835a0f216c52
orestisal97 <orestisal97@gmail.com>
2021-09-19 15:54:16 +0200

    fix the navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-import Box from './components/Projects/Projects'
-import React from 'react';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+// import styled, { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+import "./App.css";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import ReactSnapScroll from 'react-snap-scroll';
+
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="scroll-container">
+        <Navbar className="navbar" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+        <div>
+        <section id="home">
+        <h3>Hi there, I'm Orestis 👋</h3>
+        <h2>FullStack developer</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I help people and brands reach their goals by building user-centric
+          digital products and interactive experiences
         </p>
-        <Box></Box>
-      </header>
-    </div>
-  );
+        <div className="d-flex">
+          <a href="#portfolio">
+            <button type="button" className="btn btn-primary">
+              View my work
+            </button>
+          </a>
+          <a href="#contact" className="ml-3">
+            <button type="button" className="btn btn-link">
+              More About me...
+            </button>
+          </a>
+        </div>
+      </section>
+      <section id="portfolio">
+        <Projects></Projects>
+      </section>
+      <section id="contact">
+        <Contact></Contact>
+      </section>
+        </div>
+      </div>
+    );
+  }
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
 
 export default App;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Contact.module.scss';
+
+const Contact = () => (
+  <div>
+
+
+  <h2>Contact</h2>
+
+  </div>
+
+);
+
+export default Contact;
